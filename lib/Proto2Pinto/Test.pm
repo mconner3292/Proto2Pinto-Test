e2f732e2f70ffe28d2d04409cc0f77dda85470c3
Montgomery Conner <mconner@oversee.net>
2013-04-08 15:14:14 -0700

    first commit

---
+++ lib/Proto2Pinto/Test.pm
@@ -0,0 +1,18 @@
+package Proto2Pinto::Test;
+
+our $VERSION = '0.001';
+
+=pod
+
+=head1 NAME
+
+Proto2Pinto::Test - Perl/XS interface to Google's Protocol Buffers
+
+=head1 SYNOPSIS
+
+Proto2Pinto::Test is a distribution of packages that are built on-demand according
+the content of the included C<*.proto> files in this distributions C<proto/> 
+directory.
+
+=back
+
